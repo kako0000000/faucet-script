acf4820a1a18353adf74f81170d833e6eb5d7d4e
kako0000000 <kako0000000@gmail.com>
2019-11-04 17:47:27 +0500

    Update database.php

--- faucetsell/libs/database.php
+++ faucetsell/libs/database.php
@@ -2,7 +2,7 @@
 $hostname = 'localhost';
 $db_username = 'root';
 $db_password = '';
-$db_name = 'facuthubnormal';
+$db_name = 'faucet';
 
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
 
