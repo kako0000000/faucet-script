9fc370515e8999604cefa62f743b1ca494e08c87
kako0000000 <kako0000000@gmail.com>
2019-11-04 17:48:33 +0500

    Delete btcdatabase.php

--- faucetsell/libs/btcdatabase.php
+++
@@ -1,9 +0,0 @@
- <?php
-$hostname = 'localhost';
-$db_username = '1153870';
-$db_password = 'ramzan1234567890';
-$db_name = '1153870';
-
-$mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
-
-?> 
