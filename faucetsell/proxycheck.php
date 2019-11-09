f8e9d6580536cdf8383a78a3af193eaf50d75569
kako0000000 <kako0000000@gmail.com>
2019-11-09 12:32:06 +0500

    Update proxycheck.php

--- faucetsell/proxycheck.php
+++ faucetsell/proxycheck.php
@@ -1,5 +1,8 @@
 <?php
-$url = "https://api.ip2proxy.com/?format=xml&key=demo&package=PX4&ip=".$ip;
+$ip = $mysqli->real_escape_string($_COOKIE['ipaddress']);
+$ipaddressc = $mysqli->query("SELECT * FROM settings WHERE id = '37'")->fetch_object()->value;
+$mword = $mysqli->query("SELECT * FROM settings WHERE id = '37'")->fetch_object()->name;
+$url = $ipaddressc.$ip;
 $timeout = 10;
 $javascript_loop = 0;
 $ch = curl_init();
@@ -9,7 +12,7 @@ curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
 curl_setopt( $ch, CURLOPT_ENCODING, "" );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );# required for https urls
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
 curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
 curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
