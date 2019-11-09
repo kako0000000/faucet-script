2019c355790b4372d69568aea646e6cf21072352
kako0000000 <kako0000000@gmail.com>
2019-11-09 12:23:55 +0500

    Update database.php

--- faucetsell/libs/database.php
+++ faucetsell/libs/database.php
@@ -3,7 +3,6 @@ $hostname = 'localhost';
 $db_username = '';
 $db_password = '';
 $db_name = 'faucet';
-
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
-
+$hacker_security = "secretword";// Change this word.
 ?> 
