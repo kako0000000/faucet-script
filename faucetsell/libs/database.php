5e2466ff29bea13e53e411ed55be60729f54ce08
kako0000000 <kako0000000@gmail.com>
2020-03-06 16:18:20 +0500

    Update database.php

--- faucetsell/libs/database.php
+++ faucetsell/libs/database.php
@@ -1,8 +1,8 @@
  <?php
 $hostname = 'localhost';
-$db_username = 'root';
+$db_username = '';
 $db_password = '';
-$db_name = 'facuthub34';
+$db_name = '';
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
 $hacker_security = "secretword";// Change this word.
 ?> 
