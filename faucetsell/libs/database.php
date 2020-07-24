59b714c89f8f2b3260b9164e5be0dedfdb4bca06
kako0000000 <kako0000000@gmail.com>
2020-07-24 22:24:57 +0500

    Update database.php

--- faucetsell/libs/database.php
+++ faucetsell/libs/database.php
@@ -5,6 +5,6 @@ $db_password = '';
 $db_name = '';
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
 $hacker_security = "security";// Change this word.
-$admin_username = "admin"// Change this Username.
-$admin_password = "adminadmin"// Change this Password.
+$admin_username = "admin";// Change this Username.
+$admin_password = "adminadmin";// Change this Password.
 ?> 
